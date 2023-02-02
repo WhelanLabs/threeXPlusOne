463d9fdec804c06af3fec623990b23dddf039f62
John Whelan <john@whelanlabs.com>
2023-02-01 18:47:35 -0600

    better testing

--- src/test/java/com/whelanlabs/threeXPlusOne/UtilsTest.java
+++ src/test/java/com/whelanlabs/threeXPlusOne/UtilsTest.java
@@ -9,14 +9,23 @@ import org.junit.Test;
 
 public class UtilsTest {
 
+	@Test
+	public void get3XPlusOne_X0001_success() {
+		List<Integer> startingBits = Arrays.asList(0, 0, 0, 1);
+		TailArray startingTailArray = new TailArray(startingBits);
+		TailArray result = Utils.get3XPlusOne(startingTailArray);
+		assert("{ formula:\"12.0X+1.0\", base:\"(12.0X+0.0)[0, 1]\", shifts:2 }".equals(result.toString())): result.toString();
+	}
 	
 	@Test
-	public void get3XPlusOne_X0011_partB_success() {
+	public void get3XPlusOne_X0011_success() {
 		List<Integer> startingBits = Arrays.asList(0, 0, 1, 1);
 		TailArray startingTailArray = new TailArray(startingBits);
-		TailArray partBTailArray = Utils.get3XPlusOne(startingTailArray);
-		TailArray result = Utils.get3XPlusOne(partBTailArray);
-		assert("{ formula:\"9.0X+1.0\", base:\"(9.0X+2.0)[]\", shifts:3 }".equals(result.toString())): result.toString();
+		TailArray a = Utils.get3XPlusOne(startingTailArray);
+		assert("{ formula:\"24.0X+5.0\", base:\"(24.0X+0.0)[1, 0, 1]\", shifts:1 }".equals(a.toString())): a.toString();
+
+		TailArray b = Utils.get3XPlusOne(a);
+		assert("{ formula:\"9.0X+2.0\", base:\"(9.0X+2.0)[]\", shifts:3 }".equals(b.toString())): b.toString();
 	}
 	
 	@Test
@@ -38,6 +47,6 @@ public class UtilsTest {
 		assert("{ formula:\"36.0X+17.0\", base:\"(36.0X+16.0)[0, 1]\", shifts:1 }".equals(b.toString())): b.toString();
 
 		TailArray c = Utils.get3XPlusOne(b);
-		assert("failure is always an option".equals(c.toString())): c.toString();
+		assert("{ formula:\"27.0X+13.0\", base:\"(27.0X+13.0)[]\", shifts:2 }".equals(c.toString())): c.toString();
 	}
 }
